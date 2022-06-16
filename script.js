d3243e00b035091c3f6ba4f2d4de340b53dd531e
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-06-16 03:22:46 -0300

    inicio da implementacao da logica de cartas clicadas

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-//Retirada do Notion
+//funcao copiada do Notion
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-
+//array com todos os tipos de papagaio
 const types = ['bobrossparrot' , 'explodyparrot' , 'fiestaparrot' , 'metalparrot' , 'revertitparrot' , 'tripletsparrot' , 'unicornparrot'];
 
+//funcoes de criação das cartas
 function createRandomPairsList (number) {
     let list = [];
     let j = 0;
@@ -33,8 +34,7 @@ function createCards (cards) {
     }
 }
 
-
-
+//funcao que inicializa o jogo perguntando numero de cartas
 function initGame (number) {
     if (number%2 != 0 || number < 4 || number > 14 || isNaN(number))
         initGame (prompt('Quantidade de cartas (Par de 4 a 14)'));
@@ -43,5 +43,25 @@ function initGame (number) {
     createCards(cards);
 }
 
+let selected = [];
+//funcao chamada pelo clique em uma carta
+function selectCard (card) {
+    if (selected[0]==null)
+        selected[0] = card.classList;
+
+    else {
+        selected [1] = card.classList;
+        alert(selected);
+        if (selected [0] === selected[1])
+            card.remove();
+
+
+
+
+
+
+        selected = [];
+    }
+}
 
 initGame (prompt('Quantidade de cartas (Par de 4 a 14)'));
