cf054c121f88578552324a4f1a93fdba603a5612
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-06-18 01:12:45 -0300

    selecao de cartas ok, animacao de cartas virando e desvirando ok, implementado delay no clique de cartas, logica de pares corretos e errados ok, contador de número de jogadas e pares encontrados ok, fim de jogo implementado com possibilidade de reiniciar o jogo se o usuário desejar(Bonus 2) - bug de selecionar 3a carta antes do par errado desvirar ainda não corrigido

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ function comparador() {
 //array com todos os tipos de papagaio
 const types = ['bobrossparrot' , 'explodyparrot' , 'fiestaparrot' , 'metalparrot' , 'revertitparrot' , 'tripletsparrot' , 'unicornparrot'];
 
+let number = 0; //numero de cartas declarado pelo player
+let selected = []; //array duas posicoes contendo as cartas viradas na jogada atual
+let plays = 0; //numero de jogadas, vezes que se virou uma carta
+let points = 0; //numero de pares já encontrados
+let time = 0; //contador de tempo do bonus
+
 //funcoes de criação das cartas
-function createRandomPairsList (number) {
+function createRandomPairList(number) {
     let list = [];
     let j = 0;
     for (let i=0;i<number;i=list.length) {
@@ -17,8 +23,7 @@ function createRandomPairsList (number) {
     }
     return list.sort(comparador);
 }
-function createCards (cards) {
-    
+function createCards(cards) {    
     document.querySelector(".game").innerHTML = '';
     for (let i=0;i<cards.length;i++) {
         document.querySelector(".game").innerHTML += `
@@ -34,48 +39,68 @@ function createCards (cards) {
     }
 }
 
-//funcao que inicializa o jogo perguntando numero de cartas
-function initGame (number) {
-    if (number%2 != 0 || number < 4 || number > 14 || isNaN(number))
-        initGame (prompt('Quantidade de cartas (Par de 4 a 14)'));
-    
-    let cards = createRandomPairsList(number);
-    createCards(cards);
+//desvira as cartas no caso de par errado
+function unturnWrongCards() {
+    selected[0].classList.remove('turned');
+    selected[1].classList.remove('turned');
+    selected = [];
 }
 
-let selected = [];
-let plays = 0;
+
+
+//funcao que inicializa o jogo perguntando numero de cartas
+function initGame() {
+    
+    number = prompt('Quantidade de cartas (Par de 4 a 14)');
+    if (number%2 != 0 || number < 4 || number > 14 || isNaN(number))
+        initGame();
+    
+    selected = [];
+    plays = 0;
+    points = 0;
+    time = 0;
+
+    let cards = createRandomPairList(number);
+    createCards(cards);
+}
+//funcao que finaliza o jogo e reseta para o novo inicio
+function endGame () {
+    alert(`Você ganhou em ${plays} jogadas!`);
+    let reset = prompt("Reiniciar a partida?");
+    if (reset === 'sim')
+        initGame();
+    else if (reset === 'não')
+        alert("Que peninha... ;)");
+    else
+        endGame();
+}
+
+
+
 //funcao chamada pelo clique em uma carta
-//garantir nao clicar duas vezes na mesma carta
-//implementar delay antes de desvirar o par errado
-//implementar fim de jogo quando todos os pares ok
-//garantir a limpeza do array selected e funcionamento do contador plays
 function selectCard (card) {
 
-    if (!card.classList.contains("turned")) {
+    if (!card.classList.contains("turned")) { //impede o duplo click na mesma carta ou em alguma já virada
         card.classList.add('turned');
         plays++;
 
-        if (selected[0]==null)
+        if (selected[0] == null) //verifica se é a primeira virada ou a segunda
             selected[0] = card;
 
         else {
             selected [1] = card;
-            if (selected[0].classList.value === selected[1].classList.value)
-                alert('Pares!!!');
-            else {
-                
-                selected[0].classList.remove('turned');
-                selected[1].classList.remove('turned');
-                alert ('Não pares');
+
+            if (selected[0].classList.value === selected[1].classList.value) { //verifica se as viradas são pares
+                selected = [];
+                points++;
+            } else {
+                setTimeout(unturnWrongCards, 1000);
             }
 
-
-
-
-            selected = [];
+            if (points == number/2) //verifica se todos os pares já foram encontrados
+                setTimeout(endGame,500);
         }
     }
 }
 
-initGame (prompt('Quantidade de cartas (Par de 4 a 14)'));
+initGame();
